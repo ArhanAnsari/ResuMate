21a381378fc8f8dab44c52018fddd1b23d2dc7c9
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-20 18:02:58 +0530

    Update appwrite config to define client during declaration

--- src/core/api/appwrite.ts
+++ src/core/api/appwrite.ts
@@ -1,13 +1,11 @@
-import { Account, Client, Databases } from 'appwrite';
+import { Account, Client, Databases } from "appwrite";
 
 const APPWRITE_ENDPOINT = process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!;
 const APPWRITE_PROJECT_ID = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!;
 
-const client = new Client();
-
-client
-    .setEndpoint(APPWRITE_ENDPOINT)
-    .setProject(APPWRITE_PROJECT_ID);
+const client = new Client()
+  .setEndpoint(APPWRITE_ENDPOINT)
+  .setProject(APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
